90e93eacf58a7d582ac2a0925149d8bb153a2fc5
Brozi <jasiubar@gmail.com>
2025-05-26 22:15:08 +0200

    article sort add

--- src/main.js
+++ src/main.js
@@ -2,9 +2,15 @@ import './style.css'
 import { format } from "date-fns";
 const app = document.querySelector("#app");
 const form = document.getElementsByTagName("form")[0];
+const sort = document.getElementById("sort")
+const sortAction = function (e) {
+  const sortValue = sort.value;
+  return sortValue;
+}
 async function fetchArticles() {
+
   try {
-    const response = await fetch('https://vexfoiiqufvkzdavwwkh.supabase.co/rest/v1/article', {
+    const response = await fetch('https://vexfoiiqufvkzdavwwkh.supabase.co/rest/v1/article&order=${sortValue}', {
       headers: {
         apiKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZleGZvaWlxdWZ2a3pkYXZ3d2toIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0NzY1NTIxOCwiZXhwIjoyMDYzMjMxMjE4fQ.HRZX_zqR3YXrhWnqNC4BLjS8948BsOG90Xn8Cwd2K1w',
         Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZleGZvaWlxdWZ2a3pkYXZ3d2toIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0NzY1NTIxOCwiZXhwIjoyMDYzMjMxMjE4fQ.HRZX_zqR3YXrhWnqNC4BLjS8948BsOG90Xn8Cwd2K1w',
@@ -70,8 +76,10 @@ const formAction = function (e) {
   const author = document.getElementById("author").value;
   const content = document.getElementById("content").value;
   let created_at = document.getElementById("date").value;
-  created_at = new Date(created_at).toISOString()
-  createNewArticle(title, subtitle, author, content, created_at)
+  created_at = new Date(created_at).toISOString();
+  createNewArticle(title, subtitle, author, content, created_at);
+  form.reset();
 
 }
 form.addEventListener('submit', formAction);
+sort.addEventListener('change', sortAction);
