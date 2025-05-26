b6114a2cff5206dbafa1f825ca1f6bb024b34fd2
Brozi <jasiubar@gmail.com>
2025-05-26 16:33:54 +0200

    fix fetch

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import './style.css'
 
 async function fetchArticles() {
   try {
-    const response = await fetch('https://vexfoiiqufvzkzdavwwkh.supabase.co/rest/v1/articles', {
+    const response = await fetch('https://vexfoiiqufvzkzdavwwkh.supabase.co/rest/v1/article', {
       headers: {
         apiKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZleGZvaWlxdWZ2a3pkYXZ3d2toIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0NzY1NTIxOCwiZXhwIjoyMDYzMjMxMjE4fQ.HRZX_zqR3YXrhWnqNC4BLjS8948BsOG90Xn8Cwd2K1w',
       },
